41301f560fbfb5b5e3071739f410c723b545d400
KimJuHyeon <wngus1310@naver.com>
2024-01-31 22:43:19 +0900

    1

--- coders/bj_2666.py
+++ coders/bj_2666.py
@@ -3,6 +3,38 @@ import sys
 def input():
     return sys.stdin.readline()
 
-N = int(input())
-door_loc = list(map(int, input().split()))
+def Input_Data():
+    N = int(input())
+    A, B = map(int, input().split())
+    S = int(input())
+    seq = [int(input()) for _ in range(S)]
+    return N, A, B, S, seq
 
+N, A, B, S, seq = Input_Data()
+
+sol = []
+def dfs(s, left, right, sum_move):
+    if s == S:
+        sol.append(sum_move)
+        return
+
+    dfs(s + 1, seq[s], right, sum_move + abs(left - seq[s]))
+    dfs(s + 1, left, seq[s], sum_move + abs(right - seq[s]))
+
+dfs(0, min(A, B), max(A, B), 0)
+print(min(sol))
+
+
+sol = [1e99]
+def solution(s, left, right, sum_move):
+    if sum_move >= s:
+        return
+
+    if s == s:
+        sol[0] = sum_move
+        return
+
+    if seq[s] < right:
+        solution(s + 1, seq[s], right, sum_move + abs(left - seq[s]))
+    if left < seq[s]:
+        solution(s + 1, left, seq[s], sum_move + abs(right - seq[s]))
